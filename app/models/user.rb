b543fc5efdadbc371cfcb29b506d5b176b14ce79
jamcry <jamcry@hotmail.com>
2019-05-13 23:18:57 +0300

    Remove debugging lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :name,     presence: true, length: { minimum: 3 }
-  validates :email,    presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :name,     presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email,    presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
