f6dca78055051ecca0462967147a16c7c4a3b7bf
jamcry <jamcry@hotmail.com>
2019-05-14 22:37:56 +0300

    Relate user to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   attr_reader :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name,     presence: true, uniqueness: true, length: { minimum: 3 }
