964f54ea5f19a70b67a6dda60861e3a03545e44a
jamcry <jamcry@hotmail.com>
2019-05-14 22:37:31 +0300

    Add validations for post attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 150 }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:test_user)
+    @post = Post.new(title: "Example Post", body: "This is an example post body.", user: User.first)
+  end
+
+  test "title should be present" do
+    @post.title = nil
+    assert_not @post.valid?
+  end
+
+  test "title should be between 3-30 character" do
+    @post.title = "te"
+    assert_not @post.valid?
+    @post.title = "x" * 31
+    assert_not @post.valid?
+    @post.title = "This is a valid title."
+    assert @post.valid?
+  end
+
+  test "body should be present" do
+    @post.body = nil
+    assert_not @post.valid?
+  end
+
+  test "body should be between 5-150 characters" do
+    @post.body = "x" * 4
+    assert_not @post.valid?
+    @post.body = "x" * 151
+    assert_not @post.valid?
+    @post.body = "This is a valid body."
+    assert @post.valid?
+  end
+
+  test "post should belong to a user" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
 end
